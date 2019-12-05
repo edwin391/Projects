30216e4157123a9dac314f342822520a67d1432f
edwin391 <51936508+edwin391@users.noreply.github.com>
2019-12-04 19:11:33 -0600

    Update remotestart.ino

--- remotestart.ino
+++ remotestart.ino
@@ -1,81 +1,67 @@
-//remote starter sends 12V to ign 1, ign 2, acc. 
-//acc turns off during cranking
-//12 volts to starter
-//12 volts to acc after car is on.
-
 #include <SoftwareSerial.h>
-  
-
+SoftwareSerial mySerial(10,11);
+char val;
 volatile byte half_revolutions;
 unsigned int rpm;
 unsigned long timeold;
 int range = 0;
 int ign_ctrl = 5;
-int start_ctrl = 7;
 int ign2 = 6;
+int start_ctrl = 7;
 int acc_ctrl = 4;
 int rpm_sens = 2;
 
-
 void setup() {
-  Serial.begin(9600); //pin numero 2 lee las revoluciones
+  Serial.begin(9600);
+  mySerial.begin(115200);
   attachInterrupt(0, rpm_fun, RISING);
   half_revolutions = 0;
-  rpm = 0;  
+  rpm = 0;
   timeold = 0;
-  
+
   pinMode(rpm_sens, INPUT);
-  
+
   pinMode(ign_ctrl, OUTPUT);
-  pinMode(start_ctrl,OUTPUT);
-  pinMode(acc_ctrl, OUTPUT);
   pinMode(ign2, OUTPUT);
-  
+  pinMode(start_ctrl, OUTPUT);
+  pinMode(acc_ctrl, OUTPUT);
 
 }
 
 void loop() {
-  
-    if (half_revolutions >= 20) 
+  if (half_revolutions >= 20) 
   { 
      rpm = 30*1000/(millis() - timeold)*half_revolutions;
      timeold = millis();
      half_revolutions = 0;
      Serial.println(rpm,DEC);
    }
-
-switch (range) {
-  case 0:    // your hand is on the sensor
-    digitalWrite(ign_ctrl, HIGH);
-    digitalWrite(start_ctrl, LOW);
-    digitalWrite(acc_ctrl, HIGH);
-    digitalWrite(ign2, HIGH);
-    delay(2000);
-    range = 1;
-    break;
-  case 1:  
-if(rpm==0){
- 
-    digitalWrite(start_ctrl, HIGH);
-    digitalWrite(acc_ctrl, LOW);
-}
-
-range = 2;
-  
-    break;
-  case 2:  
-  
-    if(rpm >= 1000)
-    {
-    digitalWrite(acc_ctrl, HIGH);
-    digitalWrite(start_ctrl, LOW);
-    }
-    break;
+  if(mySerial.available() )
+  {
+    val = mySerial.read();
+    Serial.print(val);
   }
-  delay(1);   
-
+  switch (val) {
+    case 'H':
+      digitalWrite(ign_ctrl, HIGH);
+      digitalWrite(ign2, HIGH);
+      digitalWrite(start_ctrl, HIGH);
+      digitalWrite(acc_ctrl, HIGH);
+      delay(900);
+      digitalWrite(ign_ctrl, HIGH);
+      digitalWrite(ign2, HIGH);
+      digitalWrite(start_ctrl, LOW);
+      digitalWrite(acc_ctrl, HIGH);
+      break;
+    case 'L':
+      digitalWrite(ign_ctrl, LOW);
+      digitalWrite(ign2, LOW);
+      digitalWrite(start_ctrl, LOW);
+      digitalWrite(acc_ctrl, LOW);
+      break;
+  }
+  delay(100);
 }
-
 
 void rpm_fun()
  {
